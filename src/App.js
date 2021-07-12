56311e38530722d54c4c83a60b060b4ef9fb98de
KarenAmicone <karen_amicone@hotmail.com>
2021-07-12 18:55:20 -0500

    fetch function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import DirectoryList from "ui/components/DirectoryList";
 
 const fetchOffersList = () => {
-    return [];
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch(
+            "https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory",
+        )
+            .then(res => {
+                return res.json();
+            })
+            .then(data => setData(data.data));
+    }, []);
+    return data;
 };
 
 export default function App() {
-    return (
-        <>
-            <p class="py-4 text-lg text-center">Start here :) Good luck!</p>
-
-            <DirectoryList offerList={fetchOffersList()} />
-        </>
-    );
+    return <DirectoryList offerList={fetchOffersList()} />;
 }
