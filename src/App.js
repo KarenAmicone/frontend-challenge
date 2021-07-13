da54bb785d52753c867c32fbd4cbf849a57cd227
KarenAmicone <karen_amicone@hotmail.com>
2021-07-13 15:16:48 -0500

    Adding loading and catch error verification. Also the introduction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "./index.scss";
 import DirectoryList from "ui/components/DirectoryList";
 
 const fetchOffersList = () => {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         fetch(
             "https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory",
@@ -9,11 +12,27 @@ const fetchOffersList = () => {
             .then(res => {
                 return res.json();
             })
-            .then(data => setData(data.data));
+            .then(data => {
+                setData(data.data);
+                setIsLoading(false);
+            })
+            .catch(err => console.log(err.message));
     }, []);
-    return data;
+
+    return { data, isLoading };
 };
 
 export default function App() {
-    return <DirectoryList offerList={fetchOffersList()} />;
+    return (
+        <main>
+            <div className="intro">
+                <h1>Afiliados Reworth</h1>
+                <p>
+                    Conococe las recompensas que puedes obtener al comprar con
+                    ellos
+                </p>
+            </div>
+            <DirectoryList fetchResults={fetchOffersList()} />
+        </main>
+    );
 }
