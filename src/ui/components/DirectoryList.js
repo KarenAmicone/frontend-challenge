866a00882f5098d20ce8afee86d4af23db894a5c
KarenAmicone <karen_amicone@hotmail.com>
2021-07-13 15:18:48 -0500

    Adding function that mount components and sort

--- src/ui/components/DirectoryList.js
+++ src/ui/components/DirectoryList.js
@@ -1,5 +1,38 @@
-export default function DirectoryList({ offerList, ...props }) {
-    log.debug("Here is your offers list", offerList);
+import ListItem from "./ListItem";
 
-    return <></>;
+export default function DirectoryList({ fetchResults }) {
+    const [data, setData] = useState(fetchResults.data);
+    const sortedArray = () =>
+        fetchResults.data.sort((a, b) => {
+            return -a.discount + b.discount;
+        });
+
+    const handleClick = () => {
+        setData(sortedArray());
+        return data;
+    };
+
+    return (
+        <div>
+            {fetchResults.isLoading ? (
+                <div className="loader-container">
+                    <h2>Cargando...</h2>
+                </div>
+            ) : (
+                <div className="offers-list-container">
+                    <button onClick={handleClick} className="sort-button">
+                        Ordenar por mayor recompensa
+                    </button>
+                    <ul className="offers-list appear">
+                        {fetchResults.data.map(offer => (
+                            <ListItem
+                                key={offer.venues[0].name}
+                                offer={offer}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
 }
